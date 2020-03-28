7a718812935e189d40a130a3d6a754cefecadcb1
nygage <nygagest@gmail.com>
2020-03-28 11:47:12 +0100

    Missing mount the volume

--- coche/run.sh
+++ coche/run.sh
@@ -6,7 +6,8 @@ docker run -it\
   --device=/dev/dri:/dev/dri \
   --volume=/tmp/.X11-unix:/tmp/.X11-unix \
   ros:coche \
-  /bin/bash -c "source /root/.bashrc && cd Academy/exercises/follow_line && roslaunch ./launch/simple_line_follower_ros.launch"
+  /bin/bash -c "cd Academy/exercises/obstacle_avoidance && roslaunch ./launch/obstacle_avoidance_f1.launch & sleep 20 && cd Academy/exercises/obstacle_avoidance && python ./obstacle_avoidance_f1.py obstacle_avoidance_conf_f1.yml"
+
 
 xhost -local:root
 
